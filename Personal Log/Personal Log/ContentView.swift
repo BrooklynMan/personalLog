7f650417caf81401b468ccbb3068aa102bbd5210
adam mcbride <adam.mcbride@gmail.com>
2020-03-06 08:04:10 -0500

    initial commit

---
+++ Personal Log/Personal Log/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Personal Log
+//
+//  Created by Adam McBride on 3/6/20.
+//  Copyright © 2020 Adam McBride Designs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
